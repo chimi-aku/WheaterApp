ce24dd8230a54b6351b265ce23b9670c66a17378
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-01-02 16:03:28 +0100

    first commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button manageLocationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manageLocationButton = findViewById(R.id.ManageLocationButton);
+        manageLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openManageLocationActivity();
+            }
+        });
+    }
+
+    public void openManageLocationActivity() {
+        Intent intent = new Intent(this, ManageLocation.class);
+        startActivity(intent);
+    }
+
+}

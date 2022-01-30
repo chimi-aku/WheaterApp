01a0387fd53aa1021fd77c9864bbc6fb509ba2bb
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-01-30 11:56:56 +0100

    Change button to Imagebutton and dependencies

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button manageLocationButton;
+    private ImageButton manageLocationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
